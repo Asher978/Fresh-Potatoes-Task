1df19863d5d91adf219bc120e36613c5c9ad2853
asher978 <ashershaheen@gmail.com>
2018-03-31 16:29:06 -0400

    all tests passing

--- index.js
+++ index.js
@@ -1,82 +1,158 @@
-const sqlite = require('sqlite'),
+const sqlite3 = require('sqlite3'),
       Sequelize = require('sequelize'),
       request = require('request'),
       express = require('express'),
-      app = express();
-const sqlite3 = require('sqlite3').verbose();
-      
+      app = express(),
+      url = require('url'),
+      http = require('http'),
+      db = new sqlite3.Database("./db/database.db");
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
-
+// response to requests with error codes
+function sendError(key, code, response) {
+  response.statusCode = code;
+  response.setHeader('Content-Type', 'application/json');
+  let error = {};
+  switch (code) {
+    case 404:
+    error = { message: "key missing" };
+    break;
+    case 422:
+    error = { message: "key missing" };
+    break;
+  };
+  response.end( JSON.stringify(error) );
+};
 
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
-
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
-// incorrect url handling
-// maybe the message here should be that the user had hit an invalid route
-// however the test wriiten for this is expecting a message ({ message: 'key missing' })
-// therefore i left the message per the test
-app.get('/films/:id/recommendations/*', (req, res, next) => {
-  return res.status(404).json({ message: 'key missing' });
+// Handler for bad routes
+app.get('*', function(req, res) {
+  sendError('route', 404, res);
 });
 
-
 // ROUTE HANDLER
-function getFilmRecommendations(req, res, next) {
-  // res.status(500).send('Not Implemented');
+function getFilmRecommendations(request, response) {
 
-  /* ------------------------  PARAMS ERROR HANDLING ------------------------- */
-  const { id } = req.params;
-  const { offset, limit } = req.query;
-      
-  // req.params error handling
-  if ( isNaN(id) || id === undefined )  {
-    return res.status(422).json({ message: 'key missing' });
-  };
+  // params validation
+  let filmId = request.params.id;
+  if ( isNaN(parseInt(filmId)) ) {
+    sendError('id', 422, response);
+  }
 
-  //req.query error handling
-  if( (offset && limit) && (isNaN(offset) || isNaN(limit)) ) {
-    return res.status(422).json({ message: 'key missing' });    
-  };
-  /* -------------------------------------------------------------------------- */
-
-
-  // open database by providing the path
-  const db = new sqlite3.Database(DB_PATH, (err) => {
-    if (err) {
-      return console.error(err.message);
+  // query validations
+  let parsedQuery = url.parse(request.url, true).query;
+  // validate offset
+  let offset = parsedQuery.offset;
+  if (typeof(offset) == 'undefined') {
+    offset = 0;
+  } else {
+    if ( isNaN(parseInt(offset)) ) {
+      sendError('offset', 422, response);
     }
-    console.log('Connected to the SQlite database.');
-  });
+  }
 
-  // variable to store the db response
+  // validate limit
+  let limit = parsedQuery.limit;
+  if (typeof(limit) == 'undefined') {
+    limit = 10;
+  } else {
+    if ( isNaN(parseInt(limit)) ) {
+      sendError('limit', 422, response);
+    }
+  }
+
+
   let dbFetch = [];
-
   // inner join query that returns the same genre films filtered by + / - 15 years of the parent film
   let query = `SELECT films.id, films.title, films.release_date AS releaseDate, genres.name AS genre FROM films 
   INNER JOIN genres ON films.genre_id=genres.id 
-  WHERE genre_id = (SELECT genre_id FROM films WHERE id = ${id}) AND 
-  release_date >= date((SELECT release_date FROM films WHERE id = ${id}), '-15 years') AND
-  release_date <= date((SELECT release_date FROM films WHERE id = ${id}), '+15 years')`;
-  
-  // fetch from the DB using inner Join
+  WHERE genre_id = (SELECT genre_id FROM films WHERE id = ${filmId}) AND 
+  release_date >= date((SELECT release_date FROM films WHERE id = ${filmId}), '-15 years') AND
+  release_date <= date((SELECT release_date FROM films WHERE id = ${filmId}), '+15 years')`;
+
   db.serialize(() => {
     db.all(query, (err, rows) => {
       if (err) { console.error(err) };
-
       dbFetch = rows;
 
-    })
-  })
-  
-};
+      // API request string
+      let filmIdsQuery = '';
+      for (let i = 0; i < dbFetch.length; i++) {
+        filmIdsQuery += dbFetch[i].id.toString() + ',';
+      }
+      reviewApi = 'http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=' + filmIdsQuery;
 
+      let recommendations = []
+
+      // get the reviews for the recommended films
+      http.get(reviewApi, function(resApi) {
+        body = '';
+        resApi.on('data', function(chunk) {
+          body += chunk;
+        });
+
+        resApi.on('end', function() {
+          apiReviews = JSON.parse(body);
+
+          // 
+          for (let i = 0; i < apiReviews.length; i++) {
+            // Get number of reviews and average rating from the results
+            numReviews = apiReviews[i].reviews.length;
+
+            // check for more than or = 5 reviews
+            if (numReviews >= 5) { 
+              avgRating = 0;
+              for (let j = 0; j < numReviews; j++) {
+                avgRating += apiReviews[i].reviews[j].rating;
+              }
+              avgRating = parseFloat((avgRating / numReviews).toFixed(2));
+              
+              // check is rating is above 4.0
+              if (avgRating >= 4.0) { 
+                recommendation = {
+                  id: dbFetch[i].id,
+                  title: dbFetch[i].title,
+                  releaseDate: dbFetch[i].releaseDate,
+                  genre: dbFetch[i].genre,
+                  averageRating: avgRating,
+                  reviews: numReviews
+                };
+                recommendations.push(recommendation);
+              }
+            }
+          }
+          
+          // sort by ids
+          recommendations.sort((a, b) => a.id - b.id);
+
+          // pagination
+          recommendations = recommendations.slice(offset, offset+limit);
+
+          var fullRes = {
+            recommendations: recommendations,
+            meta: {limit: limit, offset: offset}
+          };
+
+          // repsonse to the client
+          response.statusCode = 200;
+          response.setHeader('Content-Type', 'application/json');
+          response.end( JSON.stringify(fullRes) );
+
+        });
+
+      }).on('error', function(e){
+        console.log("Got an API error: ", e);
+      });
+    });
+  }); 
+}
 
 module.exports = app;
